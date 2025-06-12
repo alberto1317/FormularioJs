70bd5108e23390bc9158dfe51bff8259e0c16c4e
alberto1317 <huper16@gmail.com>
2025-06-12 13:56:55 -0600

    Creacion Sumar.js

---
+++ FormulariosJs/Scripts/Sumar.js
@@ -0,0 +1,17 @@
+﻿var btnSumar = document.getElementById("btnSumar");
+btnSumar.onclick = function () {
+
+    var num1 = document.getElementById("txtnum1").value * 1;
+    var num2 = document.getElementById("txtnum2").value * 1;
+    var suma = num1 + num2;
+
+    document.getElementById("lblRespuesta").innerHTML = "La suma es:" + suma;
+}
+
+var btnClear = document.getElementById("btnClear");
+btnClear.onclick = function () {
+
+        document.getElementById("txtnum1").value = "";
+        document.getElementById("txtnum2").value = "";
+        document.getElementById("lblRespuesta").innerHTML = "";
+    }
